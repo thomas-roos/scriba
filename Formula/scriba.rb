bc5848ccf45394829661953e9fd5aba8205d21d6
GitHub Action <action@github.com>
2025-08-28 13:37:39 +0000

    feat: update Homebrew formula to v0.9.0

--- Formula/scriba.rb
+++ Formula/scriba.rb
@@ -1,20 +1,20 @@
 class Scriba < Formula
   desc "Modern CLI tool for recording and transcribing audio using OpenAI Whisper"
   homepage "https://github.com/giovannialberto/scriba"
-  version "0.8.1"
-
+  version "0.9.0"
+  
   if Hardware::CPU.intel?
-    url "https://github.com/giovannialberto/scriba/releases/download/v0.8.1/scriba-x86_64-apple-darwin"
-    sha256 "9efecab5ea78276c5ece620db555852ced1a5e8e0f2adfd3731bce1269184d4b"
+    url "https://github.com/giovannialberto/scriba/releases/download/v0.9.0/scriba-x86_64-apple-darwin"
+    sha256 "0dd059964e50ab9992c69e6af806ae1aca454264f04dfc2abba10fe1128c7424"
   else
-    url "https://github.com/giovannialberto/scriba/releases/download/v0.8.1/scriba-aarch64-apple-darwin"
-    sha256 "a95f5d22516869531cab4b071d4aca092b1cfcfb75ce720e80483807ce397ccd"
+    url "https://github.com/giovannialberto/scriba/releases/download/v0.9.0/scriba-aarch64-apple-darwin"
+    sha256 "0500e0d1887a664dced23b0063a9f5e10495b9677901ded4e630d6d374ad69a6"
   end
-
+  
   def install
     bin.install "scriba-#{Hardware::CPU.intel? ? "x86_64" : "aarch64"}-apple-darwin" => "scriba"
   end
-
+  
   test do
     assert_match version.to_s, shell_output("#{bin}/scriba --version")
   end
